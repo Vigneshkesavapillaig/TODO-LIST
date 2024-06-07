35883e660aaf52ec1ae1c88cee3500b1857d5484
vickey <vigneshkesavapillaig@gmail.com>
2024-06-07 22:17:18 +0900

    initail commit

--- toDoNode/db.js
+++ toDoNode/db.js
@@ -6,13 +6,13 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.CONNECTION, {
-      serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
-      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      serverSelectionTimeoutMS: 5000, 
+      socketTimeoutMS: 45000, 
     });
     console.log("MongoDB connected...");
   } catch (err) {
     console.error("MongoDB connection error:", err);
-    process.exit(1); // Exit process with failure
+    process.exit(1);
   }
 };
 
